0f52dc3ea18ac7d6638a69e74ee3f7ce03764295
Tuong Ngo <19127312@student.hcmus.edu.vn>
2023-03-05 11:36:06 +0700

    Init ChatGPT screen

---
+++ lib/main.dart
@@ -0,0 +1,151 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ChatPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+const backgroundColor = Color(0xff343541);
+const botBackgroundColor = Color(0xff444654);
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _textController = TextEditingController();
+  final srlController = ScrollController();
+  late bool isLoading;
+
+  @override
+  void initState() {
+    super.initState();
+    isLoading = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 50,
+        title: const Center(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Voice Chat GPT",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(247, 247, 248, 255),
+      ),
+      backgroundColor: backgroundColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Visibility(
+              visible: isLoading,
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CircularProgressIndicator(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  textInput(),
+                  buttonSubmit(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buttonSubmit() {
+    return Visibility(
+      visible: !isLoading,
+      child: Container(
+        color: botBackgroundColor,
+        child: IconButton(
+          icon: const Icon(
+            Icons.send_rounded,
+            color: Color.fromRGBO(142, 142, 160, 1),
+          ),
+          onPressed: () {
+            var input = _textController.text;
+            _textController.clear();
+          },
+        ),
+      ),
+    );
+  }
+
+  Expanded textInput() {
+    return Expanded(
+      child: TextField(
+        textCapitalization: TextCapitalization.sentences,
+        style: const TextStyle(color: Colors.white),
+        controller: _textController,
+        decoration: const InputDecoration(
+          fillColor: botBackgroundColor,
+          filled: true,
+          border: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          enabledBorder: InputBorder.none,
+          errorBorder: InputBorder.none,
+          disabledBorder: InputBorder.none,
+        ),
+      ),
+    );
+  }
+
+  void srllMessageList() {
+    srlController.animateTo(
+      srlController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
+  }
+}
+
+class ChatMessageWidget extends StatelessWidget {
+  const ChatMessageWidget({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      padding: const EdgeInsets.all(16),
+    );
+  }
+}
